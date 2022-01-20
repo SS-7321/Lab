f39cc20b6ea5cd5efaf9016e7f591d90fb8ff96a
SS-7321 <93590091+SS-7321@users.noreply.github.com>
2022-01-20 12:18:42 +0000

    Update simulation.py

--- simulation.py
+++ simulation.py
@@ -8,28 +8,36 @@ from PyQt5 import QtCore
 from PyQt5.QtCore import Qt
 
 def FaradayFunction(Freq,Amp,Ns,Np):
-    Vp = Amp/np.sqrt(2)
-    Vs = (Vp*Ns)/Ns
-    return 
+    eff = 0.000822 # efficiency of 1 ferrite core as a decimal
+    Rp = 18 # resistance of the circuit in the primary coil
+    Rs = 27 # resistance of the circuit in the secondary coil
+    Vp = Amp*np.sin(2*np.pi*Freq*t)
+    Vs = (Vp*Ns)/Np
+    Ip = Vp/Rp
+    Is = (eff*Vp*Ip)/Vs
+    return [Ip, Is]
 
 def update():
     Freq = float(tbox_Freq.text())
     Amp = float(tbox_Amp.text())
     Np = float(tbox_Np.text())
     Ns = float(tbox_Ns.text())
-    y = np.sin(2*np.pi*Freq*x)*Amp
-    line.set_data(x, y)
+    Ip = FaradayFunction(Freq,Amp,Ns,Np)[0]
+    Is = FaradayFunction(Freq,Amp,Ns,Np)[1]
+    primary_ax.set_data(t, Ip)
+    secondary_ax.set_data(t, Is)
+    ax.set_ylim([1.2*Ip.max(),1.2*Ip.min()])
     fig.canvas.draw_idle()
 
 
 fig, ax = pl.subplots()
-x = np.linspace(0, 10, 200)
-line, = ax.plot(x, 0*x)
-#ax.set_xlim([])
+t = np.linspace(0, 10, 200)
+primary_ax, = ax.plot(t, 0*t, label='Primary coil', color = 'red')
+secondary_ax, = ax.plot(t, 0*t, label='Secondary coil', color = 'blue')
 
 
-#Frequency input
 root = fig.canvas.manager.window
+#Frequency input
 panel_Freq = QtWidgets.QWidget()
 hbox_Freq = QtWidgets.QHBoxLayout(panel_Freq)
 tbox_Freq = QtWidgets.QLineEdit(parent = panel_Freq)
