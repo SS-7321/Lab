c7f212fddf23e9f9dc897ceec94b5a0ca0682695
SS-7321 <93590091+SS-7321@users.noreply.github.com>
2022-02-14 21:10:36 +0000

    Update FaradayRes.py

--- FaradayRes.py
+++ FaradayRes.py
@@ -27,53 +27,70 @@ def multunc(x,stdx,y,stdy,z):
 
 #%%
 
-
-
-pr_noferrite = np.loadtxt("C:/Users/User/Documents/Labs/Faraday/fr.csv", delimiter=',', skiprows=3, )
-pr_1ferrite = np.array([0.736089, 0.680556, 0.568889, 0.533889, 0.467222, 0.405000, 0.320000, 0.293889, 0.245000, 0.222222, 0.160556, 0.108889, 0.080000, 0.067222, 0.055556, 0.046006, 0.029606])
-pr_2ferrite = np.array([0.732050, 0.561800, 0.486756, 0.317339, 0.228939, 0.182006, 0.145800, 0.101250, 0.066006, 0.038272, 0.016806, 0.005339])
-sd_noferrite = np.array([2.15E-05, 1.98E-05, 1.83E-05, 1.60E-05, 1.38E-05, 1.21E-05, 1.08E-05, 8.33E-06, 6.45E-06, 4.98E-06, 3.27E-06, 2.55E-06, 1.33E-06, 8.18E-07, 7.50E-07])
-sd_1ferrite = np.array([0.000593, 0.000547, 0.000489, 0.000434, 0.000383, 0.000329, 0.000261, 0.000237, 0.000200, 0.000169, 0.000120, 0.000084, 0.000066, 0.000055, 0.000043, 0.000034, 0.000022])
-sd_2ferrite = np.array([0.000962424, 0.000725926, 0.000624001, 0.000402907, 0.000286815, 0.000226490, 0.000180446, 0.000123735, 0.000080428, 0.000045890, 0.000020454, 0.000006650])
-
-prresstd = 18*0.05
-sdresstd = 27*0.05
-prvstd = 0.01
-sdvstd = 0.1
-
+pr_0V = np.loadtxt("C:\\Users\\User\\Documents\\Labs\\Faraday\\fr.csv", delimiter=',', skiprows=3, usecols=1, max_rows=15)
+pr_1V = np.loadtxt("C:\\Users\\User\\Documents\\Labs\\Faraday\\fr.csv", delimiter=',', skiprows=3, usecols=3, max_rows=17)
+pr_2V = np.loadtxt("C:\\Users\\User\\Documents\\Labs\\Faraday\\fr.csv", delimiter=',', skiprows=3, usecols=5, max_rows=12)
+sd_0V = np.loadtxt("C:\\Users\\User\\Documents\\Labs\\Faraday\\fr.csv", delimiter=',', skiprows=22, usecols=1, max_rows=15)*10**-3
+sd_1V = np.loadtxt("C:\\Users\\User\\Documents\\Labs\\Faraday\\fr.csv", delimiter=',', skiprows=22, usecols=3, max_rows=17)*10**-3
+sd_2V = np.loadtxt("C:\\Users\\User\\Documents\\Labs\\Faraday\\fr.csv", delimiter=',', skiprows=22, usecols=5, max_rows=12)*10**-3
 
 
 #%%
-fit_efficiencynf, cov_efficiencynf = np.polyfit(pr_noferrite, sd_noferrite, 1, cov=True)
-p_efficiencynf = np.poly1d(fit_efficiencynf)
 
-fit_efficiency1f, cov_efficiency1f = np.polyfit(pr_1ferrite, sd_1ferrite, 1, cov=True)
+prvstd = 0.01
+sdvstd = 0.1
+
+pr_r = 18
+sd_r = 27
+
+prrstd = pr_r*0.05
+sdrstd = sd_r*0.05
+
+#%%
+pr_0 = pr_0V**2/pr_r
+pr_1 = pr_1V**2/pr_r
+pr_2 = pr_2V**2/pr_r
+
+sd_0 = sd_0V**2/sd_r
+sd_1 = sd_1V**2/sd_r
+sd_2 = sd_2V**2/sd_r
+
+sd0std = multunc(sd_0V**2, multunc(sd_0V,sdvstd,sd_0V,sdvstd,sd_0V**2), sd_r, sdrstd, sd_0)
+sd1std = multunc(sd_1V**2, multunc(sd_1V,sdvstd,sd_1V,sdvstd,sd_1V**2), sd_r, sdrstd, sd_1)
+sd2std = multunc(sd_2V**2, multunc(sd_2V,sdvstd,sd_2V,sdvstd,sd_2V**2), sd_r, sdrstd, sd_2)
+
+
+#%%
+fit_efficiency0f, cov_efficiency0f = np.polyfit(pr_0, sd_0, 1, w=sd0std, cov=True)
+p_efficiency0f = np.poly1d(fit_efficiency0f)
+
+fit_efficiency1f, cov_efficiency1f = np.polyfit(pr_1, sd_1, 1, w=sd1std, cov=True)
 p_efficiency1f = np.poly1d(fit_efficiency1f)
 
-fit_efficiency2f, cov_efficiency2f = np.polyfit(pr_2ferrite, sd_2ferrite, 1, cov=True)
+fit_efficiency2f, cov_efficiency2f = np.polyfit(pr_2, sd_2, 1, w=sd2std, cov=True)
 p_efficiency2f = np.poly1d(fit_efficiency2f)
 
-efficiencynf = fit_efficiencynf[0]
+efficiency0f = fit_efficiency0f[0]
 efficiency1f = fit_efficiency1f[0]
 efficiency2f = fit_efficiency2f[0]
 
-sig0nf = np.sqrt(cov_efficiencynf[0,0])
+sig00f = np.sqrt(cov_efficiency0f[0,0])
 sig01f = np.sqrt(cov_efficiency1f[0,0])
 sig02f = np.sqrt(cov_efficiency2f[0,0])
 #%%
 
-plt.plot(pr_noferrite, sd_noferrite, 'x', mew=1, ms=5, label='No Ferrite', color='blue')
-plt.plot(pr_1ferrite, sd_1ferrite, 'x', mew=1, ms=5, label='1 Ferrite Core', color='red')
-plt.plot(pr_2ferrite, sd_2ferrite, 'x', mew=1, ms=5, label='2 Ferrite Cores', color='green')
+plt.plot(pr_0, sd_0, 'x', mew=1, ms=5, label='No Ferrite', color='blue')
+plt.plot(pr_1, sd_1, 'x', mew=1, ms=5, label='1 Ferrite Core', color='red')
+plt.plot(pr_2, sd_2, 'x', mew=1, ms=5, label='2 Ferrite Cores', color='green')
 plt.title('Powers of Coils')
 plt.xlabel('Power of pr coil (W)')
 plt.ylabel('Power of sd coil (W)')
 plt.legend()
-plt.plot(pr_noferrite, p_efficiencynf(pr_noferrite), '-')
-plt.plot(pr_1ferrite, p_efficiency1f(pr_1ferrite), '-', color='red')
-plt.plot(pr_2ferrite, p_efficiency2f(pr_2ferrite), '-', color='lime')
+plt.plot(pr_0, p_efficiency0f(pr_0), '-')
+plt.plot(pr_1, p_efficiency1f(pr_1), '-', color='red')
+plt.plot(pr_2, p_efficiency2f(pr_2), '-', color='lime')
 #plt.savefig("C:/Users/User/Desktop/Faraday.png", dpi=1200)
 #%%
-print(f'Efficiency with no ferrite cores: {efficiencynf*100} +/- {sig0nf*100} %')
+print(f'Efficiency with no ferrite cores: {efficiency0f*100} +/- {sig00f*100} %')
 print(f'Efficiency with 1 ferrite core: {efficiency1f*100} +/- {sig01f} %')
 print(f'Efficiency with 2 ferrite cores: {efficiency2f*100} +/- {sig02f} %')
